09932caeeb81ad7906ec1685cddd0877f30b5d5b
Inga Michneviciute <ingeramich@gmail.com>
2021-01-15 20:49:13 +0100

    mapbox feature fixed

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -1,44 +1,46 @@
-import React, { useRef, useEffect } from "react";
-// import mapboxgl from "mapbox-gl";
-// mapboxgl.accessToken =
-//     "pk.eyJ1IjoiZGFuaWVsbXVycGh5IiwiYSI6ImNranR2MXljcTA5MHIyd281cTFrMjF0YmYifQ.-QXpFJdrgYchVUxbdAF8Aw";
-// import "./Map.css";
+import React from "react";
+
+import ReactMapboxGl, { Layer, Feature, Marker } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./Map.css";
+import { MAP_KEY } from "../../../secrets.json";
+const Page = ReactMapboxGl({
+    accessToken: MAP_KEY,
+});
 
 const Map = (props) => {
-    const mapRef = useRef();
-
-    const { center, zoom } = props;
-
-    useEffect(() => {
-        // const map = new window.google.maps.Map(mapRef.current, {
-        //   center: center,
-        //   zoom: zoom
-        // });
-        // new window.google.maps.Marker({ position: center, map: map });
-        // const map = new mapboxgl.Map({
-        //     container: "map",
-        //     style: "mapbox://styles/mapbox/dark-v10",
-        //     center: center,
-        //     zoom: zoom,
-        // });
-        // new mapboxgl.Marker()
-        //     .setLngLat(center)
-        //     .addTo(map)
-        //     .setPopup(
-        //         new mapboxgl.Popup({ offset: 25 })
-        //         //.setHTML()
-        //     )
-        //     .addTo(map);
-    }, [center, zoom]);
+    const { center } = props;
+    const lat = center.lat;
+    const lng = center.lng;
 
     return (
-        <div
-            ref={mapRef}
-            className={`map ${props.className}`}
-            // style={props.style}
-            id="map"
-            style="width: 400px; height: 300px"
-        ></div>
+        <Page
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+                height: "100%",
+                width: "100%",
+            }}
+            center={[lng, lat]}
+            zoom={[14]}
+        >
+            <Marker coordinates={[lng, lat]} anchor="bottom">
+                <img
+                    src={
+                        "https://lakelandescaperoom.com/wp-content/uploads/2019/11/google-map-icon-transparent-19.png"
+                    }
+                    width={"25px"}
+                    height={"25px"}
+                    alt={"geocach location"}
+                />
+            </Marker>
+            <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "marker-15" }}
+            >
+                <Feature coordinates={[lng, lat]} />
+            </Layer>
+        </Page>
     );
 };
 
